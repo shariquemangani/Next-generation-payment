25347597da7241f49ddf39d7d1b95fa408b2a6cf
Sharique Mangani <shariquemangani03@gmail.com>
2023-09-01 12:35:03 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -20,3 +20,13 @@ document.addEventListener("DOMContentLoaded", () => {
   counter("count2", 0, 230, maxDuration, maxDuration);
   counter("count3", 0, 230, maxDuration, maxDuration);
 });
+
+
+const blob = document.querySelector("#blob");
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove", function (dets) {
+  let half = blob.offsetWidth / 2;
+  blob.style.left = `${dets.x - half}px`;
+  blob.style.top = `${dets.y - half}px`;
+});
